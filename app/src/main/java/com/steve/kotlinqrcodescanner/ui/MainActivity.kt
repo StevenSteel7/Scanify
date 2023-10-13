fdd3cca9b825c23dd4a13b5ea750b74cb62cc8e7
Steven Moses <soggsteven92@gmail.com>
2023-10-13 23:47:29 +0530

    first

---
+++ app/src/main/java/com/steve/kotlinqrcodescanner/ui/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.steve.kotlinqrcodescanner.ui
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.steve.kotlinqrcodescanner.R
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
