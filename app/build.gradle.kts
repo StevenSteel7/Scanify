d236da4c15e74aa9831d895078570f22f0a95090
Steven Moses <soggsteven92@gmail.com>
2023-10-14 23:48:00 +0530

    zingadded

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -79,5 +79,8 @@ dependencies {
 
 
     implementation("com.journeyapps:zxing-android-embedded:4.3.0")
-    implementation("com.google.zxing:core 3.4.1")
+    implementation("com.google.zxing:core:3.4.1")
+
+
+    
 }
